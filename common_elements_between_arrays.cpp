211cc65eb6b8905627b6ea9e3e1c7d310eab272d
Shreya Gupta <shreya.gupta13606@gmail.com>
2024-01-03 00:40:31 +0530

    init

---
+++ common_elements_between_arrays.cpp
@@ -0,0 +1,41 @@
+/*
+2956. Find Common Elements Between Two Arrays
+Easy
+You are given two 0-indexed integer arrays nums1 and nums2 of sizes n and m, respectively.
+
+Consider calculating the following values:
+
+The number of indices i such that 0 <= i < n and nums1[i] occurs at least once in nums2.
+The number of indices i such that 0 <= i < m and nums2[i] occurs at least once in nums1.
+Return an integer array answer of size 2 containing the two values in the above order.
+
+ 
+
+Example 1:
+
+Input: nums1 = [4,3,2,3,1], nums2 = [2,2,5,2,3,6]
+Output: [3,4]
+Explanation: We calculate the values as follows:
+- The elements at indices 1, 2, and 3 in nums1 occur at least once in nums2. So the first value is 3.
+- The elements at indices 0, 1, 3, and 4 in nums2 occur at least once in nums1. So the second value is 4.
+*/
+class Solution {
+public:
+    vector<int> findIntersectionValues(vector<int>& nums1, vector<int>& nums2) {
+        int c1 = 0, c2 = 0;
+        for(int x : nums1)
+            for(int y : nums2)
+             if(x == y){
+                ++c1;
+                break;
+             }
+            for(int y: nums2)
+             for(int x : nums1)
+             if(x==y){
+                 ++c2;
+                 break;
+             }
+        return {c1,c2};
+        
+    }
+};
