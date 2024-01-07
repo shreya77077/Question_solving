4a7f9e8b5b72fa7c9c0b34841e6a00ef5ca4122b
Shreya Gupta <shreya.gupta13606@gmail.com>
2024-01-07 23:51:51 +0530

    init

---
+++ palindrom_in_string.cpp
@@ -0,0 +1,48 @@
+/*
+5. Longest Palindromic Substring
+Medium
+28.4K
+1.7K
+Companies
+Given a string s, return the longest 
+palindromic
+ 
+substring
+ in s.
+ */
+class Solution {
+public:
+     string longestPalindrome(string s) 
+{   
+    int len = s.size();
+    int dp[len][len];
+    memset(dp,0,sizeof(dp));
+    int end=1;
+    int start=0;
+	
+    for(int i=0;i<len;i++)
+    {
+        dp[i][i] = 1;
+    }
+    for(int i=0;i<len-1;i++)
+    {
+        if(s[i]==s[i+1])
+        { dp[i][i+1]=1;start=i;end=2;}
+    }
+    
+    for(int j=2;j<len;j++)
+    {
+        for(int i=0;i< len-j;i++)
+        {  
+            int left=i; //start point
+            int right = i+j;  //ending point
+            
+            if(dp[left+1][right-1]==1 && s[left]==s[right]) 
+            {
+                dp[left][right]=1; start=i; end=j+1; 
+            }        
+        }
+    }
+   return s.substr(start, end);
+}
+};
--- palindrome.cpp
+++ palindrome.cpp
@@ -21,3 +21,13 @@ public:
         
     }
 };
+Given a string s, return the longest 
+palindromic substring in s.
+
+ 
+
+Example 1:
+
+Input: s = "babad"
+Output: "bab"
+Explanation: "aba" is also a valid answer.
