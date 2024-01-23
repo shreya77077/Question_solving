a52f8bc02c72bb2ed2636426d9c15950fc004caa
Shreya Gupta <shreya.gupta13606@gmail.com>
2024-01-23 23:45:54 +0530

    init

--- add_binary.cpp
+++ add_binary.cpp
@@ -1,3 +1,16 @@
+/*67. Add Binary
+Given two binary strings a and b, return their sum as a binary string.
+
+Example 1:
+
+Input: a = "11", b = "1"
+Output: "100"
+Example 2:
+
+Input: a = "1010", b = "1011"
+Output: "10101"
+*/
+
 class Solution {
 public:
     // Function to add two binary numbers represented as strings
