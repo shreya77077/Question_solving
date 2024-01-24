9bf108fa92858ccfbf92af116e15ec95c7b08b21
Shreya Gupta <shreya.gupta13606@gmail.com>
2024-01-25 02:47:27 +0530

    init

--- sum.cpp
+++ sum.cpp
@@ -14,13 +14,13 @@ public:
         unordered_map<int, int> numMap;
         int n = nums.size();
 
-        for (int i = 0; i < n; i++) {
-            int complement = target - nums[i];
-            if (numMap.count(complement)) {
-                return {numMap[complement], i};
-            }
-            numMap[nums[i]] = i;
-        }
+       for(int i = 0;i<n-1; i++){
+           for(int j = i+1;j<n;j++){
+               if(nums[i] + nums[j] == target){
+                   return {i,j};
+               }
+           }
+       }
 
         return {}; // No solution found
     }
